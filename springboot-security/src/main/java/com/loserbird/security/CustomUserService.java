f1b1f04921c5db22f525efa8f09c9054fee11c4c
BINGQIN <923230014@qq.com>
2017-09-10 23:39:58 +0800

    springboot security demo

---
+++ springboot-security/src/main/java/com/loserbird/security/CustomUserService.java
@@ -0,0 +1,25 @@
+package com.loserbird.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.loserbird.dao.SysUserRepository;
+import com.loserbird.domain.SysUser;
+
+public class CustomUserService implements UserDetailsService {
+	@Autowired
+	SysUserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) { 
+		
+		SysUser user = userRepository.findByUsername(username); 
+		if(user == null){
+			throw new UsernameNotFoundException("用户名不存在");
+		}
+		
+		return user; 
+	}
+}
